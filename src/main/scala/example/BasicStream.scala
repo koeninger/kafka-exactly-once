6d4fc1d3be47828339182244f10253de6e8c264a
cody koeninger <cody@koeninger.org>
2015-05-01 12:08:48 -0500

    make basic stream runnable

--- src/main/scala/example/BasicStream.scala
+++ src/main/scala/example/BasicStream.scala
@@ -1,18 +1,23 @@
 package example
 
 object BasicStream {
+  def main(args: Array[String]) {
     import kafka.serializer.StringDecoder
     import org.apache.spark.SparkConf
     import org.apache.spark.streaming.{Seconds, StreamingContext}
     import org.apache.spark.streaming.kafka.KafkaUtils
 
-    val ssc = new StreamingContext(new SparkConf, Seconds(60))
+    val ssc = new StreamingContext(new SparkConf, Seconds(5))
 
-    val kafkaParams = Map("metadata.broker.list" -> "localhost:9092,anotherhost:9092")
+    val kafkaParams = Map("metadata.broker.list" -> "localhost:9092,localhost:9093,localhost:9094")
 
-    val topics = Set("sometopic", "anothertopic")
+    val topics = args.toSet
 
     val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topics)
 
+    stream.print
+
+    ssc.awaitTermination()
+  }
 }
