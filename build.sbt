e58948de9388a375b61659ea2cd8e8fed934b821
cody koeninger <cody@koeninger.org>
2016-03-03 16:57:02 -0600

    start hacking on kafka 0.9

--- build.sbt
+++ build.sbt
@@ -2,9 +2,11 @@ import AssemblyKeys._
 
 name := "kafka-exactly-once"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.7"
 
-val sparkVersion = "1.3.0"
+version := "2.0.0-SNAPSHOT"
+
+val sparkVersion = "2.0.0-SNAPSHOT"
 
 externalResolvers ++= Seq(
   "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository"
@@ -12,15 +14,15 @@ externalResolvers ++= Seq(
 
 libraryDependencies ++= Seq(
   ("org.apache.spark" %% "spark-core" % sparkVersion % "provided").
-    exclude("org.apache.spark", "spark-network-common_2.10").
-    exclude("org.apache.spark", "spark-network-shuffle_2.10"),
+    exclude("org.apache.spark", "spark-network-common_2.11").
+    exclude("org.apache.spark", "spark-network-shuffle_2.11"),
   // avoid an ivy bug
   "org.apache.spark" %% "spark-network-common" % sparkVersion % "provided",
   "org.apache.spark" %% "spark-network-shuffle" % sparkVersion % "provided",
   ("org.apache.spark" %% "spark-streaming" % sparkVersion % "provided").
-    exclude("org.apache.spark", "spark-core_2.10"),
-  ("org.apache.spark" %% "spark-streaming-kafka" % sparkVersion).
-    exclude("org.apache.spark", "spark-core_2.10"),
+    exclude("org.apache.spark", "spark-core_2.11"),
+  ("org.apache.spark" %% "spark-streaming-kafka-beta" % sparkVersion).
+    exclude("org.apache.spark", "spark-core_2.11"),
   ("org.scalikejdbc" %% "scalikejdbc" % "2.2.1").
     exclude("org.slf4j", "slf4j-api"),
   ("org.postgresql" % "postgresql" % "9.3-1101-jdbc4").
--- src/main/scala/example/BasicStream.scala
+++ src/main/scala/example/BasicStream.scala
@@ -1,22 +1,32 @@
 package example
 
+import kafka.serializer.StringDecoder
+import org.apache.spark.{ SparkConf, TaskContext }
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka.{ KafkaUtils, OffsetRange, HasOffsetRanges }
+import com.typesafe.config.ConfigFactory
+import java.net.InetAddress
+
 object BasicStream {
   def main(args: Array[String]): Unit = {
-    import kafka.serializer.StringDecoder
-    import org.apache.spark.SparkConf
-    import org.apache.spark.streaming.{Seconds, StreamingContext}
-    import org.apache.spark.streaming.kafka.KafkaUtils
-
+    val conf = ConfigFactory.load
+    val kafkaParams = Map(
+      "metadata.broker.list" -> conf.getString("kafka.brokers")
+    )
+    val topics = conf.getString("kafka.topics").split(",").toSet
     val ssc = new StreamingContext(new SparkConf, Seconds(5))
-
-    val kafkaParams = Map("metadata.broker.list" -> "localhost:9092,localhost:9093,localhost:9094")
-
-    val topics = args.toSet
-
     val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topics)
 
-    stream.print()
+    stream.transform { rdd =>
+      val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      rdd.mapPartitions { iter =>
+        val osr: OffsetRange = offsetRanges(TaskContext.get.partitionId)
+        val host = InetAddress.getLocalHost().getHostName()
+        val count = iter.size
+        Seq(s"${host} ${osr.topic} ${osr.partition} ${count}").toIterator
+      }.sortBy(x => x)
+    }.print()
 
     ssc.start()
     ssc.awaitTermination()
