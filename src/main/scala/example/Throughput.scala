f6a9cfe694596f1de28d5b60fc84317168c22c91
cody koeninger <cody@koeninger.org>
2016-04-05 13:37:40 -0500

    simple throughput measure, config batch time

---
+++ src/main/scala/example/Throughput.scala
@@ -0,0 +1,29 @@
+package example
+
+import kafka.serializer.DefaultDecoder
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Milliseconds, StreamingContext}
+import org.apache.spark.streaming.kafka.KafkaUtils
+import com.typesafe.config.ConfigFactory
+
+object Throughput {
+  def main(args: Array[String]): Unit = {
+    val conf = ConfigFactory.load
+
+    val ssc = new StreamingContext(new SparkConf, Milliseconds(conf.getLong("batchDurationMs")))
+
+    val kafkaParams = Map("metadata.broker.list" -> conf.getString("kafka.brokers"))
+
+    val topics = conf.getString("kafka.topics").split(",").toSet
+
+    val stream = KafkaUtils.createDirectStream[Array[Byte], Array[Byte], DefaultDecoder, DefaultDecoder](
+      ssc, kafkaParams, topics)
+
+    stream.foreachRDD { rdd =>
+      println(rdd.map(_._2.size).reduce(_+_))
+    }
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
